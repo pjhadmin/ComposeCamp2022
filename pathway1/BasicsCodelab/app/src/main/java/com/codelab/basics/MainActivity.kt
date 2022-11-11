47f41792d25f9c799a272077fb35ac45f818ce13
박주형 <juhyung.park@naverz-corp.com>
2022-11-11 09:51:57 +0900

    [pathway1] 7. State in Compose

--- pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
+++ pathway1/BasicsCodelab/app/src/main/java/com/codelab/basics/MainActivity.kt
@@ -12,6 +12,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -42,21 +44,27 @@ private fun MyApp(
 
 @Composable
 private fun Greeting(name: String) {
+    val expaneded = remember { mutableStateOf(false) }
+    val extraPadding = if (expaneded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row (modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
-                Text(text = "Hello,")
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+            ) {
+                Text(text = "Hello, ")
                 Text(text = name)
             }
-
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(
+                onClick = { expaneded.value = !expaneded.value }
+            ) {
+                Text(if (expaneded.value) "Show less" else "Show more")
             }
         }
-
     }
 }
 
